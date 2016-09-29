174aa121a5a9854189a04c48396b302c9e5e8791
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-29 16:23:21 -0700

    adds validation, not working

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,6 @@
-class Speaker < ApplicationRecord
+class Speaker < ActiveRecord::Base
+  validates :email, presence: true
+  def speechify(blabber_duration=1)
+    FFaker::HipsterIpsum.sentence(blabber_duration)
+  end
 end
